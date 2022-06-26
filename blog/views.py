dc6a91c69df8d01089c6e35e00c524f529345666
Francis Hephzibah <89093685+Hepziba97@users.noreply.github.com>
2022-06-26 19:58:43 +0100

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -12,22 +12,31 @@ from .models import Post
 
 class PostListView(ListView):
     model = Post
+    template_name = 'blog/post_list.html'
+
 
 class PostCreateView(CreateView):
     model = Post
+    template_name = 'blog/post_form.html'
     fields =  '__all__'
     success_url  = reverse_lazy('blog:all')
 
+
 class PostDetailView(DetailView):
     model = Post
+    template_name = 'blog/post_detail.html'
+
 
 class PostUpdateView(UpdateView):
     model = Post
+    template_name = 'blog/post_form.html'
     fields = '__all__'
     success_url  = reverse_lazy('blog:all')
 
+
 class PostDeleteView(DeleteView):
     model = Post
+    template_name = 'blog/post_confirm_delete.html'
     fields = '__all__'
     success_url  = reverse_lazy('blog:all')
 
